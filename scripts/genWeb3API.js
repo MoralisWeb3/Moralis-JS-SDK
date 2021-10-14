271fca8582e97c579d0f7b5026db6b106e16bb35
Shalom Ogunshola <shalomogunshola@gmail.com>
2021-10-14 12:30:15 +0100

    use web3api without server url.

--- scripts/genWeb3API.js
+++ scripts/genWeb3API.js
@@ -9,6 +9,11 @@ const DEEP_INDEX_SWAGGER_PATH = '/api-docs/v2/swagger.json';
 const OUTPUT_DIRECTORY = '../src';
 const OUTPUT_FILENAME = 'MoralisWeb3Api.js';
 
+const BodyParamTypes = {
+  setBody: 'set body',
+  property: 'property',
+};
+
 let content = '';
 
 content += `/**
@@ -21,39 +26,93 @@ content += `const axios = require('axios');\n`;
 
 content += `
 class Web3Api {
-  static initialize(serverUrl, Moralis = null) {
-    this.serverUrl = serverUrl;
+  static baseURL = 'https://deep-index.moralis.io/api/v2';
+  static BodyParamTypes = {
+    setBody: 'set body',
+    property: 'property',
+  };
+  static initialize(apiKey, Moralis = null) {
+    this.apiKey = apiKey;
     this.Moralis = Moralis;
   }
 
-  static async apiCall(name, options) {
-    if (!this.serverUrl) {
-      throw new Error('Web3Api not initialized, run Moralis.start() first');
-    }
-
-    if(this.Moralis) {
-      const { web3 } = this.Moralis;
-      
-      if (!options.address && web3) {
-        options.address = await (await web3.eth.getAccounts())[0];
-      }
-    }
-
-    try {
-      const http = axios.create({ baseURL: this.serverUrl });
-      if (!options.chain) options.chain = 'eth';
-      
-      const response =  await http.post(\`/functions/\${name}\`, options, {
-        headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-      });
-      return response.data.result
-    } catch (error) {
-      if (error.response) { 
-        throw error.response.data;
-      }
-      throw error;
-    }
+    static getBody(params, bodyParams) {
+  if (!params || !bodyParams || !bodyParams.length) {
+    return undefined;
   }
+  let body = {};
+  bodyParams.forEach(({ key, type, required }) => {
+    if (params[key] === undefined) {
+      if (required) throw new Error(\`param \${key} is required!\`);
+    } else if (type === this.BodyParamTypes.setBody) {
+      body = params[key];
+    } else {
+      body[key] = params[key];
+    }
+    // remove the param so it doesn't also get added as a query param
+    delete params[key];
+  });
+  return body;
+}
+
+static getParameterizedUrl(url, params) {
+  if (!Object.keys(params).length) return url;
+
+  // find url params, they start with :
+  const requiredParams = url.split('/').filter(s => s && s.includes(':'));
+  if (!requiredParams.length) return url;
+
+  let parameterizedUrl = url;
+  requiredParams.forEach(p => {
+    // strip the : and replace with param value
+    const key = p.substr(1);
+    const value = params[key];
+    if (!value) {
+      throw new Error(\`required param \${key} not provided\`);
+    }
+    parameterizedUrl = parameterizedUrl.replace(p, value);
+
+    // remove required param from param list
+    // so it doesn't become part of the query params
+    delete params[key];
+  });
+
+  return parameterizedUrl;
+}
+
+static getErrorMessage(error, url) {
+  return (
+    error?.data?.error ||
+    error?.data ||
+    error?.mesage ||
+    error?.toString() ||
+   \`Web3 API error while calling \${url}\`
+  );
+}
+
+static async fetch({ endpoint, params }) {
+  const { method = 'GET', url, bodyParams } = endpoint;
+  try {
+    const parameterizedUrl = this.getParameterizedUrl(url, params);
+    const body = this.getBody(params, bodyParams);
+    const http = axios.create({
+      baseURL: this.baseURL,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-api-key': this.apiKey,
+      },
+    });
+    const response = http[method.toLowerCase()](parameterizedUrl, body, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    const msg = this.getErrorMessage(error, url);
+    throw new Error(msg);
+  }
+}
+
 `;
 
 const fetchSwaggerJson = async () => {
@@ -99,6 +158,9 @@ const fetchEndpoints = async () => {
       group: item.data.tags[0],
       name: x,
       url: item.pathName.split('{').join(':').split('}').join(''),
+      bodyParams: item.data.requestBody
+        ? [{ key: 'data', type: BodyParamTypes.setBody, required: item.data.requestBody.required }]
+        : undefined,
     };
 
     data.push(endpoint);
@@ -109,24 +171,31 @@ const fetchEndpoints = async () => {
 
 const genWebApi = async () => {
   console.log('Start generating automatic Web3API code...');
-  let wrappers = {};
+  const wrappers = {};
   const ENDPOINTS = await fetchEndpoints();
 
-  ENDPOINTS.forEach(x => {
-    wrappers = {};
-  });
-
   for (const endpoint of ENDPOINTS) {
-    const { group, name } = endpoint;
+    const { group, name, url, method } = endpoint;
     if (!wrappers[group]) wrappers[group] = {};
-    wrappers[group][name] = { name };
+    wrappers[group][name] = { name, url, method };
   }
+
+  //   console.log(ENDPOINTS);
+  //   console.log(wrappers);
+
+  // ENDPOINTS.forEach(x => {
+  //   wrappers = {};
+  // });
 
   Object.keys(wrappers).forEach(group => {
     content += '\n';
     content += `  static ${group} = {\n`;
     Object.values(wrappers[group]).forEach(func => {
-      content += `    ${func.name}: async (options = {}) => Web3Api.apiCall('${func.name}', options),\n`;
+      content += `    ${
+        func.name
+      }: async (options = {}) => Web3Api.fetch({ endpoint: ${JSON.stringify(
+        ENDPOINTS.find(e => e.name === func.name)
+      )}, params: options }),\n`;
     });
     content += '  }\n';
   });
--- src/MoralisWeb3Api.js
+++ src/MoralisWeb3Api.js
@@ -5,87 +5,141 @@
 const axios = require('axios');
 
 class Web3Api {
-  static initialize(serverUrl, Moralis = null) {
-    this.serverUrl = serverUrl;
+  static baseURL = 'https://deep-index.moralis.io/api/v2';
+  static BodyParamTypes = {
+    setBody: 'set body',
+    property: 'property',
+  };
+  static initialize(apiKey, Moralis = null) {
+    this.apiKey = apiKey;
     this.Moralis = Moralis;
   }
 
-  static async apiCall(name, options) {
-    if (!this.serverUrl) {
-      throw new Error('Web3Api not initialized, run Moralis.start() first');
-    }
-
-    if(this.Moralis) {
-      const { web3 } = this.Moralis;
-      
-      if (!options.address && web3) {
-        options.address = await (await web3.eth.getAccounts())[0];
-      }
-    }
-
-    try {
-      const http = axios.create({ baseURL: this.serverUrl });
-      if (!options.chain) options.chain = 'eth';
-      
-      const response =  await http.post(`/functions/${name}`, options, {
-        headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-      });
-      return response.data.result
-    } catch (error) {
-      if (error.response) { 
-        throw error.response.data;
-      }
-      throw error;
-    }
+    static getBody(params, bodyParams) {
+  if (!params || !bodyParams || !bodyParams.length) {
+    return undefined;
   }
+  let body = {};
+  bodyParams.forEach(({ key, type, required }) => {
+    if (params[key] === undefined) {
+      if (required) throw new Error(`param ${key} is required!`);
+    } else if (type === this.BodyParamTypes.setBody) {
+      body = params[key];
+    } else {
+      body[key] = params[key];
+    }
+    // remove the param so it doesn't also get added as a query param
+    delete params[key];
+  });
+  return body;
+}
+
+static getParameterizedUrl(url, params) {
+  if (!Object.keys(params).length) return url;
+
+  // find url params, they start with :
+  const requiredParams = url.split('/').filter(s => s && s.includes(':'));
+  if (!requiredParams.length) return url;
+
+  let parameterizedUrl = url;
+  requiredParams.forEach(p => {
+    // strip the : and replace with param value
+    const key = p.substr(1);
+    const value = params[key];
+    if (!value) {
+      throw new Error(`required param ${key} not provided`);
+    }
+    parameterizedUrl = parameterizedUrl.replace(p, value);
+
+    // remove required param from param list
+    // so it doesn't become part of the query params
+    delete params[key];
+  });
+
+  return parameterizedUrl;
+}
+
+static getErrorMessage(error, url) {
+  return (
+    error?.data?.error ||
+    error?.data ||
+    error?.mesage ||
+    error?.toString() ||
+   `Web3 API error while calling ${url}`
+  );
+}
+
+static async fetch({ endpoint, params }) {
+  const { method = 'GET', url, bodyParams } = endpoint;
+  try {
+    const parameterizedUrl = this.getParameterizedUrl(url, params);
+    const body = this.getBody(params, bodyParams);
+    const http = axios.create({
+      baseURL: this.baseURL,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-api-key': this.apiKey,
+      },
+    });
+    const response = http[method.toLowerCase()](parameterizedUrl, body, {
+      params,
+    });
+    return response.data;
+  } catch (error) {
+    const msg = this.getErrorMessage(error, url);
+    throw new Error(msg);
+  }
+}
+
 
   static native = {
-    getBlock: async (options = {}) => Web3Api.apiCall('getBlock', options),
-    getDateToBlock: async (options = {}) => Web3Api.apiCall('getDateToBlock', options),
-    getLogsByAddress: async (options = {}) => Web3Api.apiCall('getLogsByAddress', options),
-    getNFTTransfersByBlock: async (options = {}) => Web3Api.apiCall('getNFTTransfersByBlock', options),
-    getTransaction: async (options = {}) => Web3Api.apiCall('getTransaction', options),
-    getContractEvents: async (options = {}) => Web3Api.apiCall('getContractEvents', options),
-    runContractFunction: async (options = {}) => Web3Api.apiCall('runContractFunction', options),
+    getBlock: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"native","name":"getBlock","url":"/block/:block_number_or_hash"}, params: options }),
+    getDateToBlock: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"native","name":"getDateToBlock","url":"/dateToBlock"}, params: options }),
+    getLogsByAddress: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"native","name":"getLogsByAddress","url":"/:address/logs"}, params: options }),
+    getNFTTransfersByBlock: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"native","name":"getNFTTransfersByBlock","url":"/block/:block_number_or_hash/nft/transfers"}, params: options }),
+    getTransaction: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"native","name":"getTransaction","url":"/transaction/:transaction_hash"}, params: options }),
+    getContractEvents: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"POST","group":"native","name":"getContractEvents","url":"/:address/events","bodyParams":[{"key":"data","type":"set body","required":false}]}, params: options }),
+    runContractFunction: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"POST","group":"native","name":"runContractFunction","url":"/:address/function","bodyParams":[{"key":"data","type":"set body","required":true}]}, params: options }),
   }
 
   static account = {
-    getTransactions: async (options = {}) => Web3Api.apiCall('getTransactions', options),
-    getNativeBalance: async (options = {}) => Web3Api.apiCall('getNativeBalance', options),
-    getTokenBalances: async (options = {}) => Web3Api.apiCall('getTokenBalances', options),
-    getTokenTransfers: async (options = {}) => Web3Api.apiCall('getTokenTransfers', options),
-    getNFTs: async (options = {}) => Web3Api.apiCall('getNFTs', options),
-    getNFTTransfers: async (options = {}) => Web3Api.apiCall('getNFTTransfers', options),
-    getNFTsForContract: async (options = {}) => Web3Api.apiCall('getNFTsForContract', options),
+    getTransactions: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"account","name":"getTransactions","url":"/:address"}, params: options }),
+    getNativeBalance: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"account","name":"getNativeBalance","url":"/:address/balance"}, params: options }),
+    getTokenBalances: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"account","name":"getTokenBalances","url":"/:address/erc20"}, params: options }),
+    getTokenTransfers: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"account","name":"getTokenTransfers","url":"/:address/erc20/transfers"}, params: options }),
+    getNFTs: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"account","name":"getNFTs","url":"/:address/nft"}, params: options }),
+    getNFTTransfers: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"account","name":"getNFTTransfers","url":"/:address/nft/transfers"}, params: options }),
+    getNFTsForContract: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"account","name":"getNFTsForContract","url":"/:address/nft/:token_address"}, params: options }),
   }
 
   static token = {
-    getTokenMetadata: async (options = {}) => Web3Api.apiCall('getTokenMetadata', options),
-    getTokenMetadataBySymbol: async (options = {}) => Web3Api.apiCall('getTokenMetadataBySymbol', options),
-    getTokenPrice: async (options = {}) => Web3Api.apiCall('getTokenPrice', options),
-    getTokenAdressTransfers: async (options = {}) => Web3Api.apiCall('getTokenAdressTransfers', options),
-    getTokenAllowance: async (options = {}) => Web3Api.apiCall('getTokenAllowance', options),
-    searchNFTs: async (options = {}) => Web3Api.apiCall('searchNFTs', options),
-    getAllTokenIds: async (options = {}) => Web3Api.apiCall('getAllTokenIds', options),
-    getContractNFTTransfers: async (options = {}) => Web3Api.apiCall('getContractNFTTransfers', options),
-    getNFTOwners: async (options = {}) => Web3Api.apiCall('getNFTOwners', options),
-    getNFTMetadata: async (options = {}) => Web3Api.apiCall('getNFTMetadata', options),
-    getTokenIdMetadata: async (options = {}) => Web3Api.apiCall('getTokenIdMetadata', options),
-    getTokenIdOwners: async (options = {}) => Web3Api.apiCall('getTokenIdOwners', options),
-    getWalletTokenIdTransfers: async (options = {}) => Web3Api.apiCall('getWalletTokenIdTransfers', options),
+    getTokenMetadata: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getTokenMetadata","url":"/erc20/metadata"}, params: options }),
+    getTokenMetadataBySymbol: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getTokenMetadataBySymbol","url":"/erc20/metadata/symbols"}, params: options }),
+    getTokenPrice: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getTokenPrice","url":"/erc20/:address/price"}, params: options }),
+    getTokenAdressTransfers: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getTokenAdressTransfers","url":"/erc20/:address/transfers"}, params: options }),
+    getTokenAllowance: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getTokenAllowance","url":"/erc20/:address/allowance"}, params: options }),
+    searchNFTs: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"searchNFTs","url":"/nft/search"}, params: options }),
+    getAllTokenIds: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getAllTokenIds","url":"/nft/:address"}, params: options }),
+    getContractNFTTransfers: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getContractNFTTransfers","url":"/nft/:address/transfers"}, params: options }),
+    getNFTOwners: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getNFTOwners","url":"/nft/:address/owners"}, params: options }),
+    getNFTMetadata: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getNFTMetadata","url":"/nft/:address/metadata"}, params: options }),
+    getTokenIdMetadata: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getTokenIdMetadata","url":"/nft/:address/:token_id"}, params: options }),
+    getTokenIdOwners: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getTokenIdOwners","url":"/nft/:address/:token_id/owners"}, params: options }),
+    getWalletTokenIdTransfers: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"token","name":"getWalletTokenIdTransfers","url":"/nft/:address/:token_id/transfers"}, params: options }),
   }
 
   static resolve = {
-    resolveDomain: async (options = {}) => Web3Api.apiCall('resolveDomain', options),
+    resolveDomain: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"resolve","name":"resolveDomain","url":"/resolve/:domain"}, params: options }),
   }
 
   static defi = {
-    getPairReserves: async (options = {}) => Web3Api.apiCall('getPairReserves', options),
-    getPairAddress: async (options = {}) => Web3Api.apiCall('getPairAddress', options),
+    getPairReserves: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"defi","name":"getPairReserves","url":"/:pair_address/reserves"}, params: options }),
+    getPairAddress: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"GET","group":"defi","name":"getPairAddress","url":"/:token0_address/:token1_address/pairAddress"}, params: options }),
   }
 
   static storage = {
-    uploadFolder: async (options = {}) => Web3Api.apiCall('uploadFolder', options),
+    uploadFolder: async (options = {}) => Web3Api.fetch({ endpoint: {"method":"POST","group":"storage","name":"uploadFolder","url":"/ipfs/uploadFolder","bodyParams":[{"key":"data","type":"set body","required":false}]}, params: options }),
   }
 }
 
