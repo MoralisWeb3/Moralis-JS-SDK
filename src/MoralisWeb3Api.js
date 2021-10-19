6af41a2b287612190ad5c3c45c941cf64afff90e
Shalom Ogunshola <shalomogunshola@gmail.com>
2021-10-19 02:08:49 +0100

    added beforeApicall function to check rates

--- src/MoralisWeb3Api.js
+++ src/MoralisWeb3Api.js
@@ -5,14 +5,96 @@
 const axios = require('axios');
 
 class Web3Api {
+  static UpdateOperation = {
+    increment: 'increment',
+    restart: 'restart',
+  };
+
   static initialize(serverUrl, Moralis = null) {
     this.serverUrl = serverUrl;
+    this.windowMs = 1;
+    this.requestLimit = 2;
     this.Moralis = Moralis;
+  }
+
+  static limitHit(firstRequestTime) {
+    const startDate = new Date(firstRequestTime);
+    const endDate = new Date();
+    const diff = endDate.getTime() - startDate.getTime();
+    const diffInMin = Math.floor(diff / 60000);
+    return diffInMin;
+  }
+
+  // Check db for request status of user either by IP or username
+  static async checkStatus(identifier) {
+    const currentData = await this.dbQuery(identifier);
+    const { firstRequestTime, requestCount } = currentData.attributes;
+    const limit = this.limitHit(firstRequestTime);
+    let response;
+    if (limit > this.windowMs) {
+      await this.updateDb(currentData, this.UpdateOperation.restart);
+      response = true;
+    } else if (limit < this.windowMs && requestCount < this.requestLimit) {
+      await this.updateDb(currentData, this.UpdateOperation.increment);
+      response = true;
+    } else {
+      response = false;
+    }
+    return response;
+  }
+
+  static async dbQuery(identifier) {
+    try {
+      const rateLimits = this.Moralis.Object.extend('RateLimits');
+      const query = new this.Moralis.Query(rateLimits);
+      query.equalTo('identifier', identifier);
+      const results = await query.find();
+      if (results.length === 0) {
+        const data = new rateLimits();
+        data.set('identifier', identifier);
+        data.set('requestCount', 1);
+        data.set('firstRequestTime', new Date());
+        await data.save();
+        return data;
+      }
+      return results[0];
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async updateDb(data, operation) {
+    try {
+      const result = await this.dbQuery(data.attributes.identifier);
+      if (operation === this.UpdateOperation.increment) {
+        result.set('requestCount', Number(result.attributes.requestCount) + 1);
+      } else {
+        result.set('requestCount', 1);
+        result.set('firstRequestTime', new Date());
+      }
+      await result.save();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async beforeAPIRequest() {
+    const user = this.Moralis.User.current();
+    // eslint-disable-next-line no-console
+    if (user) {
+      return this.checkStatus(user.attributes.username);
+    }
+    const ip = await axios.get('https://api.ipify.org/?format=json');
+    return this.checkStatus(ip.data.ip);
   }
 
   static async apiCall(name, options) {
     if (!this.serverUrl) {
       throw new Error('Web3Api not initialized, run Moralis.start() first');
+    }
+
+    if(!await this.beforeAPIRequest()) {
+      throw new Error('Too many requests, please try again later.');
     }
 
     if(this.Moralis) {
