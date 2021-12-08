83665e59363c7482bdf0901b229f4671fa9aeb1a
Shalom Ogunshola <shalomogunshola@gmail.com>
2021-12-08 08:13:36 +0100

    remove node warning in moralis.start

--- src/Parse.js
+++ src/Parse.js
@@ -52,7 +52,7 @@ class Moralis extends MoralisWeb3 {
     } else {
       // Node environment
       if (!moralisSecret) {
-        console.warn('Moralis.start warning: to use web3 access, moralisSecret is required');
+        // console.warn('Moralis.start warning: to have web3 access, moralisSecret is required');
       } else {
         this.moralisSecret = moralisSecret;
         const { web3ApiKey, speedyNodeApiKey } = await this.getApiKeys(moralisSecret);
