8cac41440730cdcae065ed4caf9c54f8ee0f31df
Erno Wever <erno@moralis.io>
2021-12-28 16:52:31 +0100

    ci: use semantic-release for publishing (#165)

---
+++ release.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+  branches: [
+    'main',
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+        message:
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
