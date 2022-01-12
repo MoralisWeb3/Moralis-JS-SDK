26915c2c97860b8622fd9bfc485743ed386e2408
Rayyan Alam <ralam@marchnetworks.com>
2022-01-12 18:22:58 -0500

    Moved Chains File

--- src/Parse.js
+++ src/Parse.js
@@ -310,21 +310,7 @@ Moralis.Elrond = require('./MoralisErd').default;
 Moralis.Erd = Moralis.Elrond;
 Moralis.Dot = require('./MoralisDot').default;
 Moralis.UI = require('./MoralisUI').default;
-Moralis.Chains = {
-  ETH_MAINENT: '0x1',
-  ETH_ROPSTEN: '0x3',
-  ETH_GOERLI: '0x5',
-  ETH_KOVAN: '0x2a',
-  ETH_RINKBEY: '0x4',
-  ETH_LOCALDEVCHAIN: '0x539',
-  POLYGON_MAINNET: '0x89',
-  POLYGON_MUMBAI: '0x13881',
-  BSC_MAINNET: '0x38',
-  BSC_TESTNET: '0x61',
-  AVAX_MAINNET: '0xa86a',
-  AVAX_TESTNET: '0xa869',
-  FANTOM_MAINNET: '0xfa',
-};
+Moralis.Chains = require('./Chians').default;
 Moralis._request = function (...args) {
   return CoreManager.getRESTController().request.apply(null, args);
 };
