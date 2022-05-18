8aef0093c605a54db173f3c7d52fe31cf1994e6a
Chidimma Ifeh <ifehchristabel1@gmail.com>
2022-05-18 17:24:47 +0100

    test: remove module (#357)
    * test(core(server)): remove server module
    
    * test(core(evm)): remove evm module
    
    * test(core(evmApi)): remove evmApi module
    
    Co-authored-by: Chidimma Ifeh <chidimmaifeh@Chidimmas-MacBook-Pro.local>
    Co-authored-by: Erno <erno@ivan.academy>

--- packages/test-app-all/src/Core.tsx
+++ packages/test-app-all/src/Core.tsx
@@ -6,7 +6,17 @@ import Evm from '@moralis/evm';
 
 
 export const Core1 = () => {
-
+  
+      const removeModule = (module: string) => {
+        if(module === 'Evm')
+          Moralis.Core.modules.remove('evm')
+        if(module === 'Server')
+          Moralis.Core.modules.remove('server')
+        const list = Moralis.Core.modules.list()
+        if(module === 'EmvApi')
+          Moralis.Core.modules.remove('emvApi')
+        console.log("after removing:",typeof(list))
+        
     const registerModule = (module: string) => {
         if(module === 'Evm')
           Core.registerModules([Evm]);
@@ -22,6 +32,10 @@ export const Core1 = () => {
     <div>
       <h2>Core</h2>
 
+    <button onClick= {() => removeModule('Evm')}>Remove evm module</button>
+    <button onClick= {() => removeModule('EvmApi')}>Remove evmApi module</button>
+    <button onClick= {() => removeModule('Server')}>Remove server module</button>
+      
     <button onClick={() => registerModule('Evm')}>Register evm module</button>
     <button onClick={() => registerModule('EvmApi')}>Register evmApi module</button>
     <button onClick={() => registerModule('Server')}>Register server module</button>
