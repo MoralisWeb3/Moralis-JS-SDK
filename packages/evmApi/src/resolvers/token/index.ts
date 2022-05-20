32ee473da36a8ca24c8681db5be14a55ddf4e819
Shalom Ogunshola <41871548+sogunshola@users.noreply.github.com>
2022-05-20 17:02:25 +0100

    feat(evmApi): get nft transfers from to block (#367)

--- packages/evmApi/src/resolvers/token/index.ts
+++ packages/evmApi/src/resolvers/token/index.ts
@@ -1,6 +1,7 @@
 export { getTokenAllowanceResolver } from './getTokenAllowance';
 export { getTokenPriceResolver } from './getTokenPrice';
 export { reSyncMetadataResolver } from './reSyncMetadata';
+export { getNftTransfersFromToBlockResolver } from './getNftTransfersFromToBlock';
 export { getTokenAddressTransfersResolver } from './getTokenAddressTransfers';
 export { getNFTTradesResolver } from './getNFTTrades';
 export { getNFTLowestPriceResolver } from './getNFTLowestPrice';
--- packages/test-app-all/src/EvmApi.tsx
+++ packages/test-app-all/src/EvmApi.tsx
@@ -167,6 +167,19 @@ export const EvmApi = () => {
       >
         getTokenTransfers
       </button>
+
+      <button
+        onClick={() => {
+          Moralis.EvmApi.token
+            .getNftTransfersFromToBlock({
+              address: '0x7de3085b3190b3a787822ee16f23be010f5f8686',
+              fromBlock: 1,
+            })
+            .then(console.log);
+        }}
+      >
+        getNftTransfersFromToBlock
+      </button>
     </div>
   );
 };
