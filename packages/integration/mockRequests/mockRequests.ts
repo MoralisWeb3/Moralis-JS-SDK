176cf084d9b3b352217c590247abe4f59eec1704
Chidimma Ifeh <chidimmaifeh@Chidimmas-MacBook-Pro.local>
2022-05-20 16:21:08 +0100

    test(evmApi): integration test for resolve address

--- packages/integration/mockRequests/mockRequests.ts
+++ packages/integration/mockRequests/mockRequests.ts
@@ -1,6 +1,7 @@
 import { setupServer } from 'msw/node';
 import { mockResolveDomain } from './evmApi/resolveDomain';
+import { mockResolveAddress } from './evmApi/resolveAddress';
 
-const handlers = [mockResolveDomain];
+const handlers = [mockResolveDomain, mockResolveAddress];
 
 export const mockServer = setupServer(...handlers);
