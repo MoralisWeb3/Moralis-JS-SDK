47748b7822072d1464d141e7864b21d3ebf27d60
Chidimma Ifeh <chidimmaifeh@Chidimmas-MacBook-Pro.local>
2022-05-22 17:00:43 +0100

    test(evmApi) test for get pair address

--- packages/integration/mockRequests/mockRequests.ts
+++ packages/integration/mockRequests/mockRequests.ts
@@ -1,7 +1,8 @@
 import { setupServer } from 'msw/node';
 import { mockResolveDomain } from './evmApi/resolveDomain';
 import { mockResolveAddress } from './evmApi/resolveAddress';
+import { mockGetPairAddress } from './evmApi/getPairAddress';
 
-const handlers = [mockResolveDomain, mockResolveAddress];
+const handlers = [mockResolveDomain, mockResolveAddress, mockGetPairAddress];
 
 export const mockServer = setupServer(...handlers);
