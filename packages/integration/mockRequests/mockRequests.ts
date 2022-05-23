5393fced4e7ddc21d40562be581cfeda07235907
Chidimma Ifeh <chidimmaifeh@Chidimmas-MacBook-Pro.local>
2022-05-23 12:47:04 +0100

    test(evmApi) test to get transactions for an account

--- packages/integration/mockRequests/mockRequests.ts
+++ packages/integration/mockRequests/mockRequests.ts
@@ -1,7 +1,8 @@
 import { setupServer } from 'msw/node';
 import { mockResolveDomain } from './evmApi/resolveDomain';
 import { mockResolveAddress } from './evmApi/resolveAddress';
+import { mockGetTransactions } from './evmApi/getTransactions';
 
-const handlers = [mockResolveDomain, mockResolveAddress];
+const handlers = [mockResolveDomain, mockResolveAddress, mockGetTransactions];
 
 export const mockServer = setupServer(...handlers);
