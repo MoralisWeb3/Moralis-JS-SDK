5473748012cfa06c08749cffed5759ed5ad31215
Chidimma Ifeh <chidimmaifeh@Chidimmas-MacBook-Pro.local>
2022-05-23 11:37:45 +0100

    test(evmApi) test to get balance of a native account

--- packages/integration/mockRequests/mockRequests.ts
+++ packages/integration/mockRequests/mockRequests.ts
@@ -1,7 +1,8 @@
 import { setupServer } from 'msw/node';
 import { mockResolveDomain } from './evmApi/resolveDomain';
 import { mockResolveAddress } from './evmApi/resolveAddress';
+import { mockGetNativeBalance } from './evmApi/getNativeBalance';
 
-const handlers = [mockResolveDomain, mockResolveAddress];
+const handlers = [mockResolveDomain, mockResolveAddress, mockGetNativeBalance];
 
 export const mockServer = setupServer(...handlers);
