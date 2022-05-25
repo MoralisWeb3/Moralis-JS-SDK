fdb022662f994c57d7173bb6aca2f5b570aa289b
Chidimma Ifeh <ifehchristabel1@gmail.com>
2022-05-25 15:37:04 +0100

    test(evmApi): get pair address (#378)
    * test(evmApi): integration test for resolve address
    
    * test(evmApi): using a test key for the config
    
    * test(evmApi) test for get pair address
    
    * test: fix moralisweb3 import
    
    Co-authored-by: Chidimma Ifeh <chidimmaifeh@Chidimmas-MacBook-Pro.local>
    Co-authored-by: Erno <erno@ivan.academy>

--- packages/integration/mockRequests/mockRequests.ts
+++ packages/integration/mockRequests/mockRequests.ts
@@ -1,9 +1,10 @@
 import { setupServer } from 'msw/node';
 import { mockResolveDomain } from './evmApi/resolveDomain';
 import { mockResolveAddress } from './evmApi/resolveAddress';
+import { mockGetPairAddress } from './evmApi/getPairAddress';
 import { mockGetNativeBalance } from './evmApi/getNativeBalance';
 import { mockGetTokenPrice } from './evmApi/getTokenPrice';
 
-const handlers = [mockResolveDomain, mockResolveAddress, mockGetNativeBalance, mockGetTokenPrice];
+const handlers = [mockResolveDomain, mockResolveAddress, mockGetNativeBalance, mockGetTokenPrice, mockGetPairAddress];
 
 export const mockServer = setupServer(...handlers);
