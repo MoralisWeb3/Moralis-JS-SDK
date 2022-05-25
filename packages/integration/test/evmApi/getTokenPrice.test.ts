72e4d89865ce91c7ce97daabab7c7b89636830a7
Chidimma Ifeh <chidimmaifeh@Chidimmas-MacBook-Pro.local>
2022-05-25 11:08:00 +0100

    test(evmmApi): testing get token price

---
+++ packages/integration/test/evmApi/getTokenPrice.test.ts
@@ -0,0 +1,50 @@
+import Core from '@moralis/core';
+import EvmApi from '@moralis/evm-api';
+import { MOCK_API_KEY } from '../../mockRequests/config';
+import { mockServer } from '../../mockRequests/mockRequests';
+
+describe('Moralis EvmApi', () => {
+  const server = mockServer;
+
+  beforeAll(() => {
+    Core.registerModules([EvmApi]);
+    Core.start({
+      apiKey: MOCK_API_KEY,
+    });
+
+    server.listen({
+      onUnhandledRequest: 'warn',
+    });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should get price, native price for an address', async () => {
+    const result = await EvmApi.token.getTokenPrice({
+      address: '0xEA47B64e1BFCCb773A0420247C0aa0a3C1D2E5C5',
+    });
+
+    expect(result.toJSON().usdPrice).toBe(162950.09891837105);
+    expect(result).toBeDefined();
+  });
+
+  it('should not get price for an invalid address and return an error code', async () => {
+    const failedResult = await EvmApi.token
+      .getTokenPrice({
+        address: '0xEA47B64e1BFCCb773A0420247C0aa0a3C1D2E5C5',
+      })
+      .then()
+      .catch((err) => {
+        return err;
+      });
+
+    expect(failedResult).toBeDefined();
+    expect(
+      EvmApi.token.getTokenPrice({
+        address: '0xEA47B64e1BFCCb773A0420247C0aa0a3C1D2E5d5',
+      }),
+    ).rejects.toThrowErrorMatchingInlineSnapshot(`"[C0005] Invalid address provided"`);
+  });
+});
