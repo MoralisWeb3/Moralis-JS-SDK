7544bc7275273358fa6870041548361e51c1c317
Chidimma Ifeh <ifehchristabel1@gmail.com>
2022-05-25 23:00:47 +0100

    test(evmApi): fixing test for get transactions

--- packages/integration/mockRequests/evmApi/getTransactions.ts
+++ packages/integration/mockRequests/evmApi/getTransactions.ts
@@ -1,11 +1,11 @@
 import { rest } from 'msw';
 import { EVM_API_ROOT, MOCK_API_KEY } from '../config';
 
-export const mockGetTransactionss: Record<string, string> = {
-  address: '0x7de3085b3190b3a787822ee16f23be010f5f8686',
+export const mockGetTransactionss: Record<string, number> = {
+  '0x7de3085b3190b3a787822ee16f23be010f5f8686': 404,
 };
 
-export const mockGetTransactions = rest.get(`${EVM_API_ROOT}/account/:address`, (req, res, ctx) => {
+export const mockGetTransactions = rest.get(`${EVM_API_ROOT}/:address`, (req, res, ctx) => {
   const address = req.params.address as string;
   const apiKey = req.headers.get('x-api-key');
 
@@ -22,7 +22,7 @@ export const mockGetTransactions = rest.get(`${EVM_API_ROOT}/account/:address`, 
   return res(
     ctx.status(200),
     ctx.json({
-      address: value,
+      total: value,
     }),
   );
 });
