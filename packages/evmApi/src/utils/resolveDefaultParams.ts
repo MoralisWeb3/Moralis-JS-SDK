076fd7399505da58d985b0ca681eff75d0353da5
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-05-30 14:27:15 +0100

    restructure default params options

--- packages/evmApi/src/utils/resolveDefaultParams.ts
+++ packages/evmApi/src/utils/resolveDefaultParams.ts
@@ -8,23 +8,34 @@ import core, {
 } from '@moralisweb3/core';
 
 export const resolveDefaultAddress = (address?: EvmAddressish) => {
-  const evm = core.modules.getNetwork('evm');
   if (address) {
-    return EvmAddress.create(address).lowercase;
-  } else if (!evm || !evm.account) {
+    return EvmAddress.create(address);
+  }
+  try {
+    const evm = core.modules.getNetwork('evm');
+    if (!evm || !evm.account) {
+      throw new Error();
+    }
+    return evm.account;
+  } catch (error) {
     throw new MoralisApiError({
       code: ApiErrorCode.GENERIC_API_ERROR,
       message: 'EvmApi failed: address is required',
     });
-  } else {
-    return EvmAddress.create(evm.account).lowercase;
   }
 };
 
 export const resolveDefaultChain = (chain?: EvmChainish) => {
-  const evm = core.modules.getNetwork('evm');
-  if (evm && evm.chain && !chain) {
-    return EvmChain.create(evm.chain).apiHex;
+  if (chain) {
+    return EvmChain.create(chain);
   }
-  return chain ? EvmChain.create(chain).apiHex : undefined;
+  try {
+    const evm = core.modules.getNetwork('evm');
+    if (!evm || !evm.chain) {
+      throw new Error();
+    }
+    return evm.chain;
+  } catch (error) {
+    return EvmChain.create('eth');
+  }
 };
