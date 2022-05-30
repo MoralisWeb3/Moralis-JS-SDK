1e3fd6cada8eebc44e984360d6a4dfa31a53939d
Chidimma Ifeh <ifehchristabel1@gmail.com>
2022-05-30 11:55:18 +0100

    test(evmApi): testing endpoint weight

---
+++ packages/integration/test/evmApi/endpointWeights.test.ts
@@ -0,0 +1,31 @@
+import Core from '@moralisweb3/core';
+import EvmApi from '@moralisweb3/evm-api';
+import { MOCK_API_KEY } from '../../mockRequests/config';
+import { mockServer } from '../../mockRequests/mockRequests';
+
+describe('Moralis EvmApi', () => {
+  const server = mockServer;
+
+  beforeAll(() => {
+    Core.registerModules([EvmApi]);
+    Core.start({
+      apiKey: MOCK_API_KEY,
+    });
+
+    server.listen({
+      onUnhandledRequest: 'warn',
+    });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should get the endpoint weight ', async () => {
+    const result = await EvmApi.info.endpointWeights({});
+
+    expect(result.toJSON()).toStrictEqual({ endpoint: 'getBlock' });
+    expect(result).toBeDefined();
+    expect(result).toEqual(expect.arrayContaining([]));
+  });
+});
