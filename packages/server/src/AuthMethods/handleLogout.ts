74422011ae865d7621e10144c8a6b6c7ff8b0caa
Erno <erno@ivan.academy>
2022-06-02 04:25:52 +0200

    feat: clear server data on logout

--- packages/server/src/AuthMethods/handleLogout.ts
+++ packages/server/src/AuthMethods/handleLogout.ts
@@ -5,18 +5,16 @@ interface HandleLogoutOptions {
 }
 
 export const handleLogout = async ({ server }: HandleLogoutOptions) => {
-  const user = await server.User.logOut();
+  const currentUser = await server.User.currentAsync();
 
-  if (user) {
-    // Note this function exists but not exported as type as it is a private function
-    // We need to make sure to clear the local saved server data to avoid reading user data
-    // from the cache after logout
-    // TODO: fix: this is not availalbe after signup?
-    try {
-      (user as Parse.User & { _clearServerData: () => void })._clearServerData();
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.warn('cannot clear serverdata', error.message);
+  // Clear server cache
+  try {
+    if (currentUser) {
+      (currentUser as Parse.User & { _clearServerData: () => void })._clearServerData();
     }
+  } catch (error) {
+    // Do nothing, might server data might not be available
   }
+
+  await server.User.logOut();
 };
