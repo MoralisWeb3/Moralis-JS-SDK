84532bc29c4faca1855f0394eed02466b8f3ef1c
b4rtaz <b4rtaz@gmail.com>
2022-07-05 16:50:12 +0200

    prettier fix.

--- demos/test-app-all/craco.config.js
+++ demos/test-app-all/craco.config.js
@@ -14,7 +14,7 @@ const packages = [
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
+    configure: webpackConfig => {
       const { isFound, match } = getLoader(webpackConfig, loaderByName('babel-loader'));
       if (isFound) {
         const include = Array.isArray(match.loader.include) ? match.loader.include : [match.loader.include];
