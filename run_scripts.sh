561fa8c09b5f270f80180deaaf22dd1e8d8859c4
b4rtaz <b4rtaz@gmail.com>
2022-07-05 16:48:13 +0200

    fix.

--- run_scripts.sh
+++ run_scripts.sh
@@ -16,7 +16,5 @@ yarn --cwd packages/evmApi $1
 yarn --cwd packages/server $1
 yarn --cwd packages/moralis $1
 
-export SKIP_PREFLIGHT_CHECK=true
 yarn --cwd demos/test-app-all $1
-
 yarn --cwd demos/test-node $1
