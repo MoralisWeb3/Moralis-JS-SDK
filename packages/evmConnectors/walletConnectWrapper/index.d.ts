ed400c9c99746ed346462565e558b6761ea6d4ba
Bart Tadych <b4rtaz@gmail.com>
2022-07-07 13:01:42 +0200

    feat(custom connectors): documentation for custom connectors + simplify base class. (#471)
    * documentation for custom connectors + simplify base class.

--- packages/evmConnectors/walletConnectWrapper/index.d.ts
+++ packages/evmConnectors/walletConnectWrapper/index.d.ts
@@ -4,4 +4,9 @@ export declare class WalletConnectProviderWrapper {
   constructor(opts: IWalletConnectProviderOptions);
   chainId: number;
   enable(): Promise<string[]>;
+  request(args: { method: string; params?: unknown[] | object }): Promise<unknown>;
+  on(event: string, listener: (args: never) => void): void;
+  once(event: string, listener: (args: unknown) => void): void;
+  removeListener(event: string, listener: (args: unknown) => void): void;
+  off(event: string, listener: (args: unknown) => void): void;
 }
