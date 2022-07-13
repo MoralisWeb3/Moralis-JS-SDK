108a8db081b098832fd4e398694ed5355ecde67d
Shalom Ogunshola <41871548+sogunshola@users.noreply.github.com>
2022-07-13 09:13:23 +0100

    feat(evmconnector): magic link
    * feat(evmconnector): magic link
    
    magic link connector package
    
    * add all possible options
    
    * fix lint error
    
    * WIP
    
    * perf(evmConnector): make package as small as possible
    
    * remove exposed email
    
    * add cdn build compatibility
    
    * add cdn test
    
    * use evmProvider datatype
    
    * remove unused code
    
    * change output name
    
    * proper implementation of abstract connector
    
    * fix magic build issue
    
    * fix cjs build magic-sdk
    
    * magic-sdk build issue
    
    Co-authored-by: Erno <erno@ivan.academy>

---
+++ packages/evmConnectors/EvmMagiclinkConnector/rollup.config.js
@@ -0,0 +1,9 @@
+import packageJson from './package.json';
+import { commonJs, esm, umd } from '../../../rollup.config';
+
+const umdExternalMap = {
+  '@moralisweb3/core': 'Moralis.CoreLib',
+  '@moralisweb3/evm-connector-utils': 'Moralis.EvmConnectorUtilsLib',
+};
+
+export default [commonJs(packageJson), esm(packageJson), umd('MagiclinkConnector', packageJson, umdExternalMap)];
