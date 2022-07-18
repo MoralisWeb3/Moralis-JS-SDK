b20798c2d22c742794ebd2906076671fc0c4acba
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-07-19 00:20:20 +0100

    format code

--- packages/api/src/MoralisApi.ts
+++ packages/api/src/MoralisApi.ts
@@ -2,7 +2,6 @@ import { ApiModule, MoralisCore } from '@moralisweb3/core';
 import { ApiConfigSetup } from './config/ApiConfigSetup';
 
 export class MoralisApi extends ApiModule {
-
   public constructor(moduleName: string, core: MoralisCore, baseUrl: string) {
     super(moduleName, core, baseUrl);
   }
