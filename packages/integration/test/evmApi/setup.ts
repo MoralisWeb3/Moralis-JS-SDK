7d617879a01aeabed400b673b5ec8feb45023195
b4rtaz <b4rtaz@gmail.com>
2022-07-20 01:48:06 +0200

    integration tests fixed.

--- packages/integration/test/evmApi/setup.ts
+++ packages/integration/test/evmApi/setup.ts
@@ -1,12 +1,14 @@
 import { MoralisCoreProvider } from '@moralisweb3/core';
-import MoralisEvmApi from '@moralisweb3/evm-api';
+import { MoralisApi } from '@moralisweb3/api';
+import { MoralisEvmApi } from '@moralisweb3/evm-api';
 import { MOCK_API_KEY } from '../../mockRequests/config';
 import { mockServer } from '../../mockRequests/mockRequests';
 
 export function setupEvmApi(): MoralisEvmApi {
   const core = MoralisCoreProvider.getDefault();
+  const api = MoralisApi.create(core);
   const envApi = MoralisEvmApi.create(core);
-  core.registerModule(envApi);
+  core.registerModules([api, envApi]);
   core.start({
     apiKey: MOCK_API_KEY,
   });
