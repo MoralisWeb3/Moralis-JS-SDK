3c415cb80bcf7a96b4c9f7c2a162448b49cf7842
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-07-24 22:24:34 +0100

    add to umbrella package

--- packages/moralis/src/index.ts
+++ packages/moralis/src/index.ts
@@ -1,6 +1,7 @@
 import { MoralisApi } from '@moralisweb3/api';
 import { MoralisAuth } from '@moralisweb3/auth';
 import { MoralisEvmApi } from '@moralisweb3/evm-api';
+import { MoralisSolUtils } from '@moralisweb3/sol-utils';
 import { MoralisSolApi } from '@moralisweb3/sol-api';
 import { MoralisCoreProvider } from '@moralisweb3/core';
 
@@ -8,10 +9,11 @@ const core = MoralisCoreProvider.getDefault();
 const auth = MoralisAuth.create(core);
 const api = MoralisApi.create(core);
 const evmApi = MoralisEvmApi.create(core);
+const solUtils = MoralisSolUtils.create(core);
 const solApi = MoralisSolApi.create(core);
 
 // register all Moralis modules to MoralisCore
-core.registerModules([auth, api, evmApi, solApi]);
+core.registerModules([solUtils, auth, api, evmApi, solApi]);
 
 const Moralis = {
   Core: core,
