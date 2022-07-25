7a6f7e31c29e64e707512aad4ba82b0522061ca0
b4rtaz <b4rtaz@gmail.com>
2022-07-25 10:47:31 +0200

    tests fix.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Map workspaces to their source code so that Jest can resolve them correctly.
   moduleNameMapper: {
     '^@moralisweb3/evm-api': '<rootDir>/../evmApi/src',
-    '^@moralisweb3/api': '<rootDir>/../api/src',
+    '^@moralisweb3/api-utils': '<rootDir>/../api-utils/src',
     '^@moralisweb3/core': '<rootDir>/../core/src',
     '^@moralisweb3/(.*)$': '<rootDir>/../$1/src',
   },
--- packages/integration/test/evmApi/setup.ts
+++ packages/integration/test/evmApi/setup.ts
@@ -1,4 +1,4 @@
-import { MoralisApi } from '@moralisweb3/api';
+import { MoralisApiUtils } from '@moralisweb3/api-utils';
 import { MoralisCoreProvider } from '@moralisweb3/core';
 import { MoralisEvmApi } from '@moralisweb3/evm-api';
 import { MOCK_API_KEY } from '../../mockRequests/config';
@@ -6,9 +6,9 @@ import { mockServer } from '../../mockRequests/mockRequests';
 
 export function setupEvmApi(): MoralisEvmApi {
   const core = MoralisCoreProvider.getDefault();
-  const api = MoralisApi.create(core);
+  const apiUtils = MoralisApiUtils.create(core);
   const envApi = MoralisEvmApi.create(core);
-  core.registerModules([api, envApi]);
+  core.registerModules([apiUtils, envApi]);
   core.start({
     apiKey: MOCK_API_KEY,
   });
