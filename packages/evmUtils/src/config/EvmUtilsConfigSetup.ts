cb78f30b43f414136a2dcd913eae5453e3ab5648
Erno <erno@ivan.academy>
2022-07-25 11:46:38 +0200

    fix: allow errors when registering key

--- packages/evmUtils/src/config/EvmUtilsConfigSetup.ts
+++ packages/evmUtils/src/config/EvmUtilsConfigSetup.ts
@@ -3,12 +3,7 @@ import { EvmUtilsConfig } from './EvmUtilsConfig';
 
 export class EvmUtilsConfigSetup {
   public static register(config: Config) {
-    if (!config.hasKey(EvmUtilsConfig.formatEvmAddress)) {
-      config.registerKey(EvmUtilsConfig.formatEvmAddress);
-    }
-
-    if (!config.hasKey(EvmUtilsConfig.formatEvmChainId)) {
-      config.registerKey(EvmUtilsConfig.formatEvmChainId);
-    }
+    config.registerKey(EvmUtilsConfig.formatEvmAddress);
+    config.registerKey(EvmUtilsConfig.formatEvmChainId);
   }
 }
