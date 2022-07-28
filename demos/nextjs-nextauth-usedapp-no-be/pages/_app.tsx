b2181371af482cf7ced4ffd8d5ae56e37347f907
Dmitrii <dmitrii@moralis.io>
2022-07-28 18:18:45 +0700

    feat: dependencies updated

--- demos/nextjs-nextauth-usedapp-no-be/pages/_app.tsx
+++ demos/nextjs-nextauth-usedapp-no-be/pages/_app.tsx
@@ -1,7 +1,5 @@
 import '../styles/globals.css';
-import { ethers } from 'ethers';
 import { SessionProvider } from 'next-auth/react';
-import { Web3ReactProvider } from '@web3-react/core';
 import type { AppProps } from 'next/app';
 import { Mainnet, DAppProvider, Config } from '@usedapp/core';
 import { getDefaultProvider } from 'ethers';
@@ -14,20 +12,12 @@ const config: Config = {
   autoConnect: true,
 };
 
-const getLibrary = (provider: any) => {
-  const library = new ethers.providers.Web3Provider(provider);
-  library.pollingInterval = 8000; // frequency provider is polling
-  return library;
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DAppProvider config={config}>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <SessionProvider session={pageProps.session} refetchInterval={0}>
-          <Component {...pageProps} />
-        </SessionProvider>
-      </Web3ReactProvider>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </DAppProvider>
   );
 }
