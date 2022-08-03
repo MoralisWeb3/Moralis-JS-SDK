f840e9ed97b7e6d8532742848ecd141a8535b2e5
Erno <erno@ivan.academy>
2022-08-03 11:10:33 +0200

    chore: remove unused demo files

--- demos/demo-parse-server-auth/src/auth/authRouter.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { verify, request } from './authController';
-
-export const authRouter = express.Router();
-
-authRouter.route('/request-message').post(request);
-authRouter.route('/sign-message').post(verify);
