08d7a899097c8ddca520d2281b2fe8b3c2b42097
b4rtaz <b4rtaz@gmail.com>
2022-08-10 15:07:56 +0200

    eslint fix.

--- demos/firebase-proxy/functions/.eslintrc.js
+++ demos/firebase-proxy/functions/.eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    tsconfigRootDir: __dirname,
     project: ['./tsconfig.json', './tsconfig.dev.json'],
     sourceType: 'module',
   },
