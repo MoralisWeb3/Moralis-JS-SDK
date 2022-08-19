0c9bc7dc5eb73c23416f284451d3b4e4c22590d0
b4rtaz <b4rtaz@gmail.com>
2022-08-19 11:31:18 +0200

    prettier fix.

--- packages/core/src/utils/maybe.ts
+++ packages/core/src/utils/maybe.ts
@@ -3,7 +3,10 @@
  * Optionally, the value gets transformed by the second argument
  */
 export function maybe<Value>(value: Value | null | undefined): Value | undefined;
-export function maybe<Value, Output>(value: Value | null | undefined, transform: (value: Value) => Output): Output | undefined;
+export function maybe<Value, Output>(
+  value: Value | null | undefined,
+  transform: (value: Value) => Output,
+): Output | undefined;
 export function maybe<Value, Output>(value: Value | null | undefined, transform?: (value: Value) => Output) {
   if (value == null) {
     return undefined;
