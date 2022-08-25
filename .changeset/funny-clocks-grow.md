7fa4654a39484df09af57ef07674738e5650732d
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-08-25 11:09:28 +0100

    docs: added technical docs to EvmTransaction datatype

---
+++ .changeset/funny-clocks-grow.md
@@ -0,0 +1,5 @@
+---
+'@moralisweb3/evm-utils': minor
+---
+
+Added technical documentation for EvmTransaction datatype
--- packages/evmUtils/src/dataTypes/EvmTransaction/EvmTransaction.ts
+++ packages/evmUtils/src/dataTypes/EvmTransaction/EvmTransaction.ts
@@ -5,9 +5,39 @@ import { EvmNative } from '../EvmNative';
 import { EvmTransactionLog } from '../EvmTransactionLog';
 import { EvmTransacionInput, EvmTransactionData } from './types';
 
+/**
+ * Valid input for a new EvmTransaction instance.
+ * This can be an existing {@link EvmTransaction} or a valid {@link EvmTransacionInput} object
+ */
 export type EvmTransactionish = EvmTransacionInput | EvmTransaction;
 
+/**
+ * The EvmTranaction is a representation of a published transaction.
+ *
+ * Use this class any time you work with a transaction.
+ *
+ * @category DataType
+ */
 export class EvmTransaction implements MoralisDataObject {
+  /**
+   * Create a new instance of EvmTransaction from any valid transaction input
+   * @param data - the EvmTransactionish type
+   * @example
+   * ```
+   * const transaction = EvmTransaction.create(data);
+   *```
+   */
+  static create(data: EvmTransactionish) {
+    if (data instanceof EvmTransaction) {
+      return data;
+    }
+
+    return new EvmTransaction(data);
+  }
+
+  /**
+   * Internal reference of the transaction.
+   */
   private _data: EvmTransactionData;
 
   constructor(data: EvmTransacionInput) {
@@ -43,14 +73,15 @@ export class EvmTransaction implements MoralisDataObject {
     logs: (data.logs ?? []).map((log) => EvmTransactionLog.create(log)),
   });
 
-  static create(data: EvmTransactionish) {
-    if (data instanceof EvmTransaction) {
-      return data;
-    }
-
-    return new EvmTransaction(data);
-  }
-
+  /**
+   * Check the equality between two Evm transactions
+   * @param dataA - The first transaction
+   * @param dataB - The second transaction
+   * @example
+   * ```ts
+   * EvmTransaction.equals(dataA, dataB)
+   * ```
+   */
   static equals(dataA: EvmTransactionish, dataB: EvmTransactionish) {
     const transactionA = EvmTransaction.create(dataA);
     const transactionB = EvmTransaction.create(dataB);
@@ -66,6 +97,14 @@ export class EvmTransaction implements MoralisDataObject {
     return true;
   }
 
+  /**
+   * Checks the equality of the current transaction with another evm transaction
+   * @param data - the transaction to compare with
+   * @example
+   * ```ts
+   * transaction.equals(data)
+   * ```
+   */
   equals(data: EvmTransactionish): boolean {
     return EvmTransaction.equals(this, data);
   }
@@ -86,14 +125,238 @@ export class EvmTransaction implements MoralisDataObject {
       chain: data.chain?.format(),
       contractAddress: data.contractAddress?.format(),
       logs: data.logs.map((log) => log.toJSON()),
+      blockTimestamp: data.blockTimestamp.toString(),
     };
   }
 
+  /**
+   * @returns a JSON represention of the transaction.
+   * @example
+   * ```
+   * transaction.format()
+   * ```
+   */
   format() {
     return this.toJSON();
   }
 
+  /**
+   * @returns the transaction
+   * @example
+   * ```
+   * transaction.result
+   * ```
+   */
   get result() {
     return this._data;
+  }
+
+  /**
+   * @returns the transaction to address
+   * @example
+   * ```
+   * transaction.to
+   * ```
+   */
+  get to() {
+    return this._data.to;
+  }
+
+  /**
+   * @returns the transaction from address
+   * @example
+   * ```
+   * transaction.address
+   * ```
+   */
+  get from() {
+    return this._data.from;
+  }
+
+  /**
+   * @returns the transaction nonce
+   * @example
+   * ```
+   * transaction.nonce
+   * ```
+   */
+  get nonce() {
+    return this._data.nonce;
+  }
+
+  /**
+   * @returns the transaction gas
+   * @example
+   * ```
+   * transaction.gas
+   * ```
+   */
+  get gas() {
+    return this._data.gas;
+  }
+
+  /**
+   * @returns the transaction gas price
+   * @example
+   * ```
+   * transaction.gasPrice
+   * ```
+   */
+  get gasPrice() {
+    return this._data.gasPrice;
+  }
+
+  /**
+   * @returns the transaction gas used
+   * @example
+   * ```
+   * transaction.gasUsed
+   * ```
+   */
+  get gasUsed() {
+    return this._data.gasUsed;
+  }
+
+  /**
+   * @returns the transaction cumulative gas used
+   * @example
+   * ```
+   * transaction.cumulativeGasUsed
+   * ```
+   */
+  get cumulativeGasUsed() {
+    return this._data.cumulativeGasUsed;
+  }
+
+  /**
+   * @returns the transaction block number
+   * @example
+   * ```
+   * transaction.blockNumber
+   * ```
+   */
+  get blockNumber() {
+    return this._data.blockNumber;
+  }
+
+  /**
+   * @returns the transaction value
+   * @example
+   * ```
+   * transaction.value
+   * ```
+   */
+  get value() {
+    return this._data.value;
+  }
+
+  /**
+   * @returns the transaction chain
+   * @example
+   * ```
+   * transaction.chain
+   * ```
+   */
+  get chain() {
+    return this._data.chain;
+  }
+
+  /**
+   * @returns the transaction contract address
+   * @example
+   * ```
+   * transaction.contractAddress
+   * ```
+   */
+  get contractAddress() {
+    return this._data.contractAddress;
+  }
+
+  /**
+   * @returns the transaction logs
+   * @example
+   * ```
+   * transaction.logs
+   * ```
+   */
+  get logs() {
+    return this._data.logs;
+  }
+
+  /**
+   * @returns the transaction receipt root
+   * @example
+   * ```
+   * transaction.receiptRoot
+   * ```
+   */
+  get receiptRoot() {
+    return this._data.receiptRoot;
+  }
+
+  /**
+   * @returns the transaction receipt status
+   * @example
+   * ```
+   * transaction.receiptStatus
+   * ```
+   */
+  get receiptStatus() {
+    return this._data.receiptStatus;
+  }
+
+  /**
+   * @returns the transaction data
+   * @example
+   * ```
+   * transaction.data
+   * ```
+   */
+  get data() {
+    return this._data.data;
+  }
+
+  /**
+   * @returns the transaction hash
+   * @example
+   * ```
+   * transaction.hash
+   * ```
+   */
+  get hash() {
+    return this._data.hash;
+  }
+
+  /**
+   * @returns the transaction type
+   * @example
+   * ```
+   * transaction.type
+   * ```
+   */
+  get type() {
+    return this._data.type;
+  }
+
+  /**
+   * @returns the transaction black hash
+   * @example
+   * ```
+   * transaction.blockHash
+   * ```
+   */
+  get blockHash() {
+    return this._data.blockHash;
+  }
+
+  /**
+   * @returns the transaction block timestamp
+   * @example
+   * ```
+   * transaction.blockTimestamp
+   * ```
+   */
+  get blockTimestamp() {
+    return this._data.blockTimestamp;
   }
 }
