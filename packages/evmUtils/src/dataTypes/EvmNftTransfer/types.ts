732e93b490dd05f2ef25300d429f9e4aed113378
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-09-01 12:01:49 +0100

    docs: EvmNftTransfer
    thechnical docs for nft transfer daratype

--- packages/evmUtils/src/dataTypes/EvmNftTransfer/types.ts
+++ packages/evmUtils/src/dataTypes/EvmNftTransfer/types.ts
@@ -3,6 +3,29 @@ import { EvmAddressish, EvmAddress } from '../EvmAddress';
 import { EvmChain, EvmChainish } from '../EvmChain';
 import { EvmNativeish, EvmNative } from '../EvmNative';
 
+/**
+ * This can be any object with valid transaction data.
+ * @example
+ * ```
+ * const input = {
+ *  chain: 1,
+ *  tokenAddress: "0x057Ec652A4F150f7FF94f089A38008f49a0DF88e",
+ *  toAddress: "0x057Ec652A4F150f7FF94f089A38008f49a0DF88e",
+ *  operator: null,
+ *  fromAddress: "0x057Ec652A4F150f7FF94f089A38008f49a0DF88e",
+ *  value: null,
+ *  blockTimestamp: "2021-06-04T16:00:15",
+ *  blockHash: "0x057Ec652A4F150f7FF94f089A38008f49a0DF88e",
+ *  transactionIndex: 123,
+ *  blockNumber: "88256",
+ *  contractType: "ERC721",
+ *  logIndex: 0,
+ *  tokenId: "15",
+ *  transactionHash: "0x057Ec652A4F150f7FF94f089A38008f49a0DF88e",
+ *  transactionType: "1",
+ * }
+ * ```
+ */
 export interface EvmNftTransferInput {
   chain: EvmChainish;
   amount?: null | string | number;
@@ -22,6 +45,9 @@ export interface EvmNftTransferInput {
   operator?: null | EvmAddressish;
 }
 
+/**
+ * This is the return type of the processed EVM NFT Transfer.
+ */
 export interface EvmNftTransferData {
   chain: EvmChain;
   amount?: number;
