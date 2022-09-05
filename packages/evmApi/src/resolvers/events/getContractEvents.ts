d4fa502194a74a874171d623d1b88399b4c7fcd6
b4rtaz <b4rtaz@gmail.com>
2022-09-05 13:58:40 +0200

    removed PaginatedResponse from getContractEvents.

--- packages/evmApi/src/resolvers/events/getContractEvents.ts
+++ packages/evmApi/src/resolvers/events/getContractEvents.ts
@@ -2,7 +2,6 @@ import {
   createPaginatedEndpointFactory,
   createPaginatedEndpoint,
   PaginatedParams,
-  PaginatedResult,
   EndpointBodyType,
 } from '@moralisweb3/api-utils';
 import { Camelize } from '@moralisweb3/core';
@@ -29,11 +28,8 @@ export const getContractEvents = createPaginatedEndpointFactory((core) =>
     name: 'getContractEvents',
     urlParams: ['address'],
     getUrl: (params: Params) => `/${params.address}/events`,
-    //   TODO: remove PaginatedResponse when api squad make swagger update
-    apiToResult: (data: PaginatedResult<ApiResult>, params: Params) => {
-      // TODO: swagger returns a wrong definition, we need to fix it.
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      return ((data.result as any[]) ?? [])?.map((event) =>
+    apiToResult: (data: ApiResult, params: Params) => {
+      return (data.result ?? [])?.map((event) =>
         EvmEvent.create(
           {
             chain: EvmChainResolver.resolve(params.chain, core),
