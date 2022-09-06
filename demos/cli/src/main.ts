8891d066b70bedcb625a3b621dd2dda35afcdfdb
Erno <erno@ivan.academy>
2022-09-06 15:45:20 +0200

    revert: changes in cli demo

--- demos/cli/src/main.ts
+++ demos/cli/src/main.ts
@@ -15,12 +15,14 @@ async function main() {
     apiKey: env['MORALIS_API_KEY'],
   });
 
-  const collection = await Moralis.EvmApi.nft.getWalletNFTCollections({
-    address: '0xbf48C4f51dD8C1a396386380c80EBfe667b3c1A7',
+  const block = await Moralis.EvmApi.native.getBlock({
+    blockNumberOrHash: '15305775',
     chain: '0x1',
   });
-  console.log(collection.raw);
+  console.log(block);
 
+  const weights = await Moralis.EvmApi.info.endpointWeights();
+  console.log(weights.result);
 }
 
 main();
