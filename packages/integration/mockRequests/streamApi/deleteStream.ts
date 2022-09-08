9eff875d18a035012986d49e3b04396f5cd86a09
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-09-08 10:25:06 +0100

    correct tests with params

--- packages/integration/mockRequests/streamApi/deleteStream.ts
+++ packages/integration/mockRequests/streamApi/deleteStream.ts
@@ -15,10 +15,17 @@ export const mockDeleteStreamOutput: Record<string, unknown> = {
 };
 
 export const mockDeleteStream = rest.delete(`${STREAM_API_ROOT}/streams/:id`, (req, res, ctx) => {
+  const id = req.params.id as string;
   const apiKey = req.headers.get('x-api-key');
 
   if (apiKey !== MOCK_API_KEY) {
     return res(ctx.status(401));
+  }
+
+  const value = mockDeleteStreamOutput;
+
+  if (value.id !== id) {
+    return res(ctx.status(404));
   }
 
   return res(ctx.status(200), ctx.json(mockDeleteStreamOutput));
