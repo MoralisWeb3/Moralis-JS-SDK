96cc35ad6fbf28c8575678c2084ad66bf8020335
Bart Tadych <b4rtaz@gmail.com>
2022-09-09 15:26:38 +0200

    update swagger.json. (#664)

--- packages/evmApi/src/resolvers/token/getTokenTransfers.ts
+++ packages/evmApi/src/resolvers/token/getTokenTransfers.ts
@@ -4,7 +4,8 @@ import { EvmChainish, EvmAddressish, EvmAddress, Erc20Transfer } from '@moralisw
 import { operations } from '../../generated/types';
 import { EvmChainResolver } from '../EvmChainResolver';
 
-type operation = 'getTokenAddressTransfers';
+// Old name: getTokenAddressTransfers
+type operation = 'getTokenTransfers';
 
 type QueryParams = operations[operation]['parameters']['query'];
 type PathParams = operations[operation]['parameters']['path'];
