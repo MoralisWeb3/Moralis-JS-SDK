c50db343c8f8dfffa97759f020230889c40651ac
3scava1i3r <tripathi.hritwik@gmail.com>
2022-09-13 23:28:15 +0530

    feat:adding Add SolApi.token.getTokenPrice

--- packages/solApi/src/config/SolApiConfig.ts
+++ packages/solApi/src/config/SolApiConfig.ts
@@ -1,7 +1,6 @@
 import { SolNetworkish } from '@moralisweb3/sol-utils';
 import { ConfigKey } from '@moralisweb3/core';
 
-
 export const SolApiConfig = {
   defaultSolNetwork: {
     name: 'defaultSolNetwork',
