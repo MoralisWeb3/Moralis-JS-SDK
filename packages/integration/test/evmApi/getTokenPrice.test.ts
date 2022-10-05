f4d5c12b4c6f7516082b3e182051cc649e64f763
Bart Tadych <b4rtaz@gmail.com>
2022-10-05 10:32:30 +0200

    refactor getTokenPrice tests. (#723)

--- packages/integration/test/evmApi/getTokenPrice.test.ts
+++ packages/integration/test/evmApi/getTokenPrice.test.ts
@@ -13,28 +13,31 @@ describe('getTokenPrice', () => {
   });
 
   it('returns an info', async () => {
-    const response = await evmApi.token.getTokenPrice({
-      address: '0x0000000000000000000000000000000000000001',
-    });
+    const address = '0x0000000000000000000000000000000000000001';
+    const response = await evmApi.token.getTokenPrice({ address });
 
     expect(response.result.usdPrice).toBe(0.000011961341215674);
-    expect(response.result.exchangeAddress?.lowercase).toBe('0x1f98431c8ad98523631ae4a59f267346ea31f984');
+    expect(response.result.exchangeAddress?.lowercase).toBe(address);
     expect(response.result.exchangeName).toBe('Uniswap v3');
   });
 
   it('passes chain correctly', async () => {
+    const address = '0x0000000000000000000000000000000000000002';
     const response = await evmApi.token.getTokenPrice({
-      address: '0x0000000000000000000000000000000000000002',
+      address,
       chain: 0x13881,
     });
     expect(response).toBeDefined();
+    expect(response.result.exchangeAddress?.lowercase).toBe(address);
   });
 
   it('passes toBlock correctly', async () => {
+    const address = '0x0000000000000000000000000000000000000003';
     const response = await evmApi.token.getTokenPrice({
-      address: '0x0000000000000000000000000000000000000003',
+      address,
       toBlock: 512,
     });
     expect(response).toBeDefined();
+    expect(response.result.exchangeAddress?.lowercase).toBe(address);
   });
 });
