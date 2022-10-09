b3a06d3d1873bc47cf6605245298d434114350c1
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-10-09 22:20:50 +0100

    improve test

--- packages/integration/test/evmApi/getContractLogs.test.ts
+++ packages/integration/test/evmApi/getContractLogs.test.ts
@@ -14,11 +14,18 @@ describe('getContractLogs', () => {
 
   it('should get logs for an address', async () => {
     const result = await evmApi.events.getContractLogs({
-      address: '0xa2107fa5b38d9bbd2c461d6edf11b11a50f6b974',
+      address: '0xa2107fa5b38d9bbd2c461d6edf11b11a50f6b989',
+      chain: 137, // Polygon
+      topic: '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62',
+      fromDate: '2022-03-05T13:45:42.000Z',
+      toDate: '2022-03-05T13:45:42.000Z',
+      topic0: '0x2caecd17d02f56fa897705dcc740da2d237c373f70686f4e0d9bd3bf0400ea7a',
+      topic1: '0x000000000000000000000000031002d15b0d0cd7c9129d6f644446368deae391',
+      topic2: '0x000000000000000000000000d25943be09f968ba740e0782a34e710100defae9',
     });
 
     expect(result).toBeDefined();
-    expect(result.raw.total).toBe(856189);
+    expect(result.raw.total).toBe(100);
     expect(result).toEqual(expect.objectContaining({}));
   });
 
