f680257cf8c9a95f3b315e56bdd83dd93bf01f18
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-10-25 09:24:45 +0100

    revert

--- packages/apiUtils/src/resolvers/getCommonHeaders.ts
+++ packages/apiUtils/src/resolvers/getCommonHeaders.ts
@@ -3,7 +3,7 @@ import MoralisCore from '@moralisweb3/core';
 export type Headers = { [key: string]: string };
 
 export const getCommonHeaders = (): Headers => ({
-  'x-moralis-platform': `JS SDK`,
+  'x-moralis-platform': 'JS SDK',
   'x-moralis-platform-version': MoralisCore.libVersion,
   'x-moralis-build-target': 'node',
 });
