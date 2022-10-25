b3a07c1bcc1a97d864b741676b7c84c2882514b3
Erno <erno@ivan.academy>
2022-10-25 15:59:57 +0200

    test: fix testing

--- packages/streams/src/test/setup.ts
+++ packages/streams/src/test/setup.ts
@@ -1,11 +1,11 @@
 import { MoralisCore, MoralisCoreProvider } from '@moralisweb3/core';
 import { MoralisStreams } from '../MoralisStreams';
-import { MoralisEvmUtils } from '@moralisweb3/common-evm-utils';
+import { CommonEvmUtils } from '@moralisweb3/common-evm-utils';
 
 export const setupStreams = () => {
   const core = MoralisCore.create();
   const moralisStreams = MoralisStreams.create(core);
-  const evmUtils = MoralisEvmUtils.create(core);
+  const evmUtils = CommonEvmUtils.create(core);
   core.registerModules([moralisStreams, evmUtils]);
 
   MoralisCoreProvider.setDefault(core);
