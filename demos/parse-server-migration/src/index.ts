92d9a7297f07683a490390c924e1eb5e28ea9dec
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-11-01 13:51:28 +0100

    perf(demo): parse server migration
    update parse server migration with new env option for sync

--- demos/parse-server-migration/src/index.ts
+++ demos/parse-server-migration/src/index.ts
@@ -6,6 +6,7 @@ import { parseServer } from './parseServer';
 // @ts-ignore
 import ParseServer from 'parse-server';
 import http from 'http';
+import ngrok from 'ngrok';
 import { streamsSync } from '@moralisweb3/parse-server';
 
 export const app = express();
@@ -19,19 +20,27 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(
-  streamsSync(parseServer, {
-    apiKey: config.MORALIS_API_KEY,
-    webhookUrl: '/streams',
-  }),
-);
+if (config.USE_STREAMS) {
+  app.use(
+    streamsSync(parseServer, {
+      apiKey: config.MORALIS_API_KEY,
+      webhookUrl: '/streams',
+    }),
+  );
+}
 
 app.use(`/server`, parseServer.app);
 
 const httpServer = http.createServer(app);
-httpServer.listen(config.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Moralis Server is running on port ${config.PORT}.`);
+httpServer.listen(config.PORT, async () => {
+  if (config.USE_STREAMS) {
+    const url = await ngrok.connect(config.PORT);
+    // eslint-disable-next-line no-console
+    console.log(`Moralis Streams is running on port ${config.PORT} and ngrok url ${url}`);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`Moralis Server is running on port ${config.PORT}.`);
+  }
 });
 // This will enable the Live Query real-time server
 ParseServer.createLiveQueryServer(httpServer);
