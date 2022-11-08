5a36dd03f03b37ae291ef5edd7ac79965f6e29c1
Shalom Ogunshola <shalomogunshola@gmail.com>
2022-11-08 15:23:18 +0100

    add ignoreBodyCheckOperationNames to test

--- packages/common/evmUtils/src/operations/operations.test.ts
+++ packages/common/evmUtils/src/operations/operations.test.ts
@@ -14,9 +14,25 @@ describe('operations', () => {
       it('defines all supported parameters', () => {
         const openApiPathParamNames = reader.readOperationPathParamNames(operation.id)?.map(toCamel);
         const openApiSearchParamNames = reader.readOperationSearchParamNames(operation.id)?.map(toCamel);
+        const ignoreBodyCheckOperationNames = ['abi'];
+        const openApiBodyParamNames = reader
+          .readOperationRequestBodyParamNames(operation.id)
+          ?.map(toCamel)
+          .filter((name) => !ignoreBodyCheckOperationNames.includes(name));
+
+        ignoreBodyCheckOperationNames.forEach((name) => {
+          operation.bodyParamNames?.splice(operation.bodyParamNames.indexOf(name as never), 1);
+        });
+
+        let bodyParamNames;
+
+        if (Array.isArray(operation.bodyParamNames) && operation.bodyParamNames.length > 0) {
+          bodyParamNames = operation.bodyParamNames;
+        }
 
         expect(operation.urlPathParamNames?.sort().join(',')).toBe(openApiPathParamNames?.sort().join(','));
         expect(operation.urlSearchParamNames?.sort().join(',')).toBe(openApiSearchParamNames?.sort().join(','));
+        expect(bodyParamNames?.sort().join(',')).toBe(openApiBodyParamNames?.sort().join(','));
       });
 
       it(`getRequestUrlParams() function returns all supported property names`, () => {
