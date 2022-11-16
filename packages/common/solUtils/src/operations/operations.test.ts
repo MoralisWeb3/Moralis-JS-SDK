400aa44c0eec2652cd752e751b1b780b3a29cd63
Erno <erno@ivan.academy>
2022-11-16 18:58:08 +0700

    test: fix getTokenPrice param validation test

--- packages/common/solUtils/src/operations/operations.test.ts
+++ packages/common/solUtils/src/operations/operations.test.ts
@@ -2,11 +2,6 @@ import { toCamel } from '@moralisweb3/common-core';
 import { OpenApiInterfaceReader, OperationDefinitionReader } from '@moralisweb3/test-utils';
 import { operations } from './operations';
 
-const ignoreUrlSearchCheckOperationNames = [
-  // Test fails when having multiple return statements due to validation check
-  'getTokenPrice',
-];
-
 describe('operations', () => {
   let reader: OpenApiInterfaceReader;
 
@@ -26,22 +21,19 @@ describe('operations', () => {
         expect(operation.bodyParamNames?.sort().join(',')).toBe(openApiBodyParamNames?.sort().join(','));
       });
 
-      (ignoreUrlSearchCheckOperationNames.includes(operation.name) ? it.skip : it)(
-        `getRequestUrlParams() function returns all supported property names`,
-        () => {
-          const openApiPathParamNames = reader.readOperationPathParamNames(operation.id)?.map(toCamel);
-          const openApiSearchParamNames = reader.readOperationSearchParamNames(operation.id); // Must be same as in API
+      it(`getRequestUrlParams() function returns all supported property names`, () => {
+        const openApiPathParamNames = reader.readOperationPathParamNames(operation.id)?.map(toCamel);
+        const openApiSearchParamNames = reader.readOperationSearchParamNames(operation.id); // Must be same as in API
 
-          const definitionReader = new OperationDefinitionReader(
-            `src/operations/${operation.groupName}/${operation.name}Operation.ts`,
-          );
-          const returnPropertyNames = definitionReader.getRequestUrlParamsFunctionReturnPropertyNames();
+        const definitionReader = new OperationDefinitionReader(
+          `src/operations/${operation.groupName}/${operation.name}Operation.ts`,
+        );
+        const returnPropertyNames = definitionReader.getRequestUrlParamsFunctionReturnPropertyNames();
 
-          const expectedPropertyNames = [...(openApiPathParamNames || []), ...(openApiSearchParamNames || [])];
+        const expectedPropertyNames = [...(openApiPathParamNames || []), ...(openApiSearchParamNames || [])];
 
-          expect(returnPropertyNames.sort().join(',')).toBe(expectedPropertyNames.sort().join(','));
-        },
-      );
+        expect(returnPropertyNames.sort().join(',')).toBe(expectedPropertyNames.sort().join(','));
+      });
     });
   }
 });
