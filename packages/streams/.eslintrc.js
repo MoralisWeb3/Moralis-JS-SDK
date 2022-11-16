3b6fc0915847cb74b2ec4c4d5ba4b3daf864a618
Erno <erno@ivan.academy>
2022-11-16 17:16:46 +0700

    fix: ignore openapi errors

--- packages/streams/.eslintrc.js
+++ packages/streams/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   extends: ['@moralisweb3'],
   plugins: ['jest'],
-  ignorePatterns: ['**/lib/**/*', '**/*.test.ts', '**/dist/**/*', '**/build/**/*', '**/generated/**/*'],
+  ignorePatterns: ['**/lib/**/*', '**/*.test.ts', '**/dist/**/*', '**/build/**/*', '**/generated/**/*', 'openapi.ts'],
 };
