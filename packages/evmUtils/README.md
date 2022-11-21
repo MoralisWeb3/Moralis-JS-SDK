5cc48bd2751673640520e3e8324869949f408652
Bart Tadych <b4rtaz@gmail.com>
2022-11-21 13:17:20 +0100

    revert depreciated evm-utils & sol-utils packages. (#837)
    * revert depreciated evm-utils & sol-utils packages.

---
+++ packages/evmUtils/README.md
@@ -0,0 +1,3 @@
+# @moralisweb3/evm-utils
+
+🚨 This package is depreciated. Please use the `@moralisweb3/common-evm-utils` package instead.
---
+++ packages/solUtils/src/index.ts
@@ -0,0 +1,6 @@
+export * from '@moralisweb3/common-sol-utils';
+
+// eslint-disable-next-line no-console
+console.warn(
+  'moralis: @moralisweb3/sol-utils package is depreciated. Please use @moralisweb3/common-sol-utils instead',
+);
