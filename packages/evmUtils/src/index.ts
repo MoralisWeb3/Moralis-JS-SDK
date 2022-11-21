a70a534c94a0ffb4b92c63e3d3a5f467849bf611
b4rtaz <b4rtaz@gmail.com>
2022-11-21 12:40:31 +0100

    prettier fix.

--- packages/evmUtils/src/index.ts
+++ packages/evmUtils/src/index.ts
@@ -1,4 +1,6 @@
 export * from '@moralisweb3/common-evm-utils';
 
 // eslint-disable-next-line no-console
-console.warn('moralis: @moralisweb3/evm-utils package is depreciated. Please use @moralisweb3/common-evm-utils instead');
+console.warn(
+  'moralis: @moralisweb3/evm-utils package is depreciated. Please use @moralisweb3/common-evm-utils instead',
+);
