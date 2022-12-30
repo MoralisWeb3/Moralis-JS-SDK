1703d47ca2e1248a850d9edcbd2fbcb1d02c48f1
Erno <erno@ivan.academy>
2022-12-30 17:48:54 +0700

    ci: fix yarn lock install on changeset

--- scripts/changesetRelease.sh
+++ scripts/changesetRelease.sh
@@ -1,4 +1,4 @@
 # Convert changesets into a new version
 npx changeset version
 # Update lock-file (needed to avoid invalid lock-file with outdated versions)
-npm i --package-lock-only
+yarn install
