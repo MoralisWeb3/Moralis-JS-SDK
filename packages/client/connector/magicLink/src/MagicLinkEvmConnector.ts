64197c90f5766bb734a78e99fa52d37e038b7659
FedericoAmura <federicoamura@gmail.com>
2023-01-02 14:29:49 -0300

    feat: add better typing in MagicLink Connector

--- packages/client/connector/magicLink/src/MagicLinkEvmConnector.ts
+++ packages/client/connector/magicLink/src/MagicLinkEvmConnector.ts
@@ -1,8 +1,12 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { EvmConnection, EvmConnector } from '@moralisweb3/client-evm-auth';
 import { WalletDetails } from '@moralisweb3/client-auth-utils';
-import { Magic, MagicSDKAdditionalConfiguration, MagicSDKExtensionsOption } from 'magic-sdk';
+import { Magic, MagicSDKAdditionalConfiguration } from 'magic-sdk';
 import { ConnectExtension } from '@magic-ext/connect';
+
+interface MagicSDK extends Magic {
+  connect: ConnectExtension;
+}
 
 export interface MagicLinkEvmConnectorOptions extends MagicSDKAdditionalConfiguration {}
 
@@ -12,23 +16,21 @@ export class MagicLinkEvmConnector implements EvmConnector {
   }
 
   public readonly name = 'magicLink';
-  private readonly magic: Magic<MagicSDKExtensionsOption<ConnectExtension['name']>>;
+  private readonly magic: MagicSDK;
   private readonly options: MagicLinkEvmConnectorOptions;
 
   public constructor(apiKey: string, options: MagicLinkEvmConnectorOptions = {}) {
     this.options = {
       network: 'mainnet',
       ...options,
-      extensions: [new ConnectExtension(), ...(options.extensions ?? [])],
+      extensions: [new ConnectExtension()],
     };
 
-    this.magic = new Magic<ConnectExtension['name'], MagicSDKExtensionsOption<ConnectExtension['name']>>(
-      apiKey,
-      this.options,
-    );
+    this.magic = new Magic(apiKey, this.options) as unknown as MagicSDK;
   }
 
   public async connect(): Promise<EvmConnection> {
+    this.magic.connect.disconnect();
     const provider = this.magic.rpcProvider;
     await provider.enable();
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -40,7 +42,7 @@ class MagicLinkEvmConnection implements EvmConnection {
   public constructor(
     public readonly connectorName: string,
     public readonly provider: typeof Magic.prototype.rpcProvider,
-    private readonly magic: Magic<MagicSDKExtensionsOption<ConnectExtension['name']>>,
+    private readonly magic: MagicSDK,
   ) {}
 
   public async readWallet(): Promise<WalletDetails> {
@@ -60,7 +62,6 @@ class MagicLinkEvmConnection implements EvmConnection {
   }
 
   public async disconnect(): Promise<void> {
-    // @ts-ignore
     await this.magic.connect.disconnect();
     localStorage.removeItem('magicLink');
   }
--- packages/client/connector/walletConnect/jest.config.js
+++ packages/client/connector/walletConnect/jest.config.js
@@ -1,4 +1,4 @@
 /* eslint-disable global-require */
 module.exports = {
-  ...require('../../jest.config'),
+  ...require('../../../../jest.config'),
 };
