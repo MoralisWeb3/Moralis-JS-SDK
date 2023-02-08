2ebd449ab3cc5ec37264bd50a26ac41ad9445669
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-02-08 12:34:48 +0700

    stash

--- demos/react-vite/src/App.tsx
+++ demos/react-vite/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -11,24 +11,26 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
+        <a href="https://moralis.io" target="_blank">
+          <img src="/moralis.svg" className="logo" alt="Moralis logo" />
+        </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Moralis + Vite React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div className="card">
+          <button>💸 Fetch Native Balance</button>
+          <button>💸 Fetch Native Balance</button>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
