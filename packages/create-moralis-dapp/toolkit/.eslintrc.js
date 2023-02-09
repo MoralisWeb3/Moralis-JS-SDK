ed1f93e535af6f8a7edfca6db0142cb0a03b3e3f
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-02-09 21:15:56 +0700

    refactor: create-moralis-dapp v2

---
+++ packages/create-moralis-dapp/toolkit/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@moralisweb3'],
+  plugins: ['jest'],
+  ignorePatterns: ['**/lib/**/*', '**/*.test.ts', '**/dist/**/*', '**/build/**/*', '**/generated/**/*'],
+  rules: {
+    'no-console': 'off',
+    'no-html-link-for-pages': 'off',
+  },
+  globals: {
+    NodeJS: true,
+  },
+};
