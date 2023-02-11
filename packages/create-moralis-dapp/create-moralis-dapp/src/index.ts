3701a9ef027678ea682358f189b36e1eb9367a95
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-02-11 09:34:59 +0700

    upd: cli added new template

--- packages/create-moralis-dapp/create-moralis-dapp/src/index.ts
+++ packages/create-moralis-dapp/create-moralis-dapp/src/index.ts
@@ -2,8 +2,9 @@
 import { CMDGenerator } from './CMDGenerator';
 import { NextAppGenerator } from '@create-moralis-dapp/next';
 import { ExpressAppGenerator } from '@create-moralis-dapp/express';
+import { ReactViteAppGenerator } from '@create-moralis-dapp/react-vite';
 
 async function main() {
-  return new CMDGenerator([new NextAppGenerator(), new ExpressAppGenerator()]).run();
+  return new CMDGenerator([new NextAppGenerator(), new ExpressAppGenerator(), new ReactViteAppGenerator()]).run();
 }
 main();
