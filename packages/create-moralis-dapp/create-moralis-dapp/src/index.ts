3c0367ccf5d470dd1c1b6e99b85accb6a1510f34
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-02-11 09:48:58 +0700

    cli upd

--- packages/create-moralis-dapp/create-moralis-dapp/src/index.ts
+++ packages/create-moralis-dapp/create-moralis-dapp/src/index.ts
@@ -2,8 +2,9 @@
 import { CMDGenerator } from './CMDGenerator';
 import { NextAppGenerator } from '@create-moralis-dapp/next';
 import { ExpressAppGenerator } from '@create-moralis-dapp/express';
+import { ReactCraAppGenerator } from '@create-moralis-dapp/react-cra';
 
 async function main() {
-  return new CMDGenerator([new NextAppGenerator(), new ExpressAppGenerator()]).run();
+  return new CMDGenerator([new NextAppGenerator(), new ExpressAppGenerator(), new ReactCraAppGenerator()]).run();
 }
 main();
