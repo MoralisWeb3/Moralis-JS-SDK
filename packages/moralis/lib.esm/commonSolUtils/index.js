95f1b35dd3f4d8d6941027ef2f4881b7ae627a12
Dmitry Kapeliushnyi <its.dmitrii.kpl@gmail.com>
2023-03-10 23:57:17 +0700

    returned commonJs module for CMD

---
+++ packages/moralis/lib.esm/commonSolUtils/index.js
@@ -0,0 +1,2 @@
+export * from '@moralisweb3/common-sol-utils';
+//# sourceMappingURL=index.js.map
---
+++ packages/moralis/lib.esm/index.js
@@ -0,0 +1,36 @@
+import { Streams } from '@moralisweb3/streams';
+import { ApiUtils } from '@moralisweb3/api-utils';
+import { Auth } from '@moralisweb3/auth';
+import { CommonEvmUtils } from '@moralisweb3/common-evm-utils';
+import { EvmApi } from '@moralisweb3/evm-api';
+import { CommonSolUtils } from '@moralisweb3/common-sol-utils';
+import { SolApi } from '@moralisweb3/sol-api';
+import { Core, CoreProvider } from '@moralisweb3/common-core';
+// Core
+const core = Core.create();
+// Utility modules
+const commonEvmUtils = CommonEvmUtils.create(core);
+const commonSolUtils = CommonSolUtils.create(core);
+const apiUtils = ApiUtils.create(core);
+// Feature modules
+const auth = Auth.create(core);
+const streams = Streams.create(core);
+const evmApi = EvmApi.create(core);
+const solApi = SolApi.create(core);
+// Register all Moralis modules to Core
+core.registerModules([commonEvmUtils, commonSolUtils, auth, apiUtils, evmApi, solApi, streams]);
+CoreProvider.setDefault(core);
+const Moralis = {
+    Core: core,
+    Auth: auth,
+    Streams: streams,
+    EvmApi: evmApi,
+    SolApi: solApi,
+    EvmUtils: commonEvmUtils,
+    SolUtils: commonSolUtils,
+    start: (config) => {
+        return core.start(config);
+    },
+};
+export default Moralis;
+//# sourceMappingURL=index.js.map
