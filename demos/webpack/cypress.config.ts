1e88fef7a5d675150e563011931bebf5027f04b3
b4rtaz <b4rtaz@gmail.com>
2023-03-22 14:56:24 +0100

    updated baseUrl.

--- demos/webpack/cypress.config.ts
+++ demos/webpack/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9753/e2e/webpack/public/',
+    baseUrl: 'http://localhost:9753/demos/webpack/public/',
     video: false,
   },
 });
