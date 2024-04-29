9790264e62caf2314c6d87249f186ea42f1bb656
Kayode Odole <31928403+Kayconfig@users.noreply.github.com>
2024-04-29 15:55:34 +0100

    chore: write test to validate linea-sepolia

--- packages/common/evmUtils/src/dataTypes/EvmChain/EvmChain.test.ts
+++ packages/common/evmUtils/src/dataTypes/EvmChain/EvmChain.test.ts
@@ -127,6 +127,7 @@ describe('EvmChain', () => {
     expect(EvmChain.MOONBEAM.apiHex).toBe('0x504');
     expect(EvmChain.MOONRIVER.apiHex).toBe('0x505');
     expect(EvmChain.MOONBASE.apiHex).toBe('0x507');
+    expect(EvmChain.LINEA_SEPOLIA.apiHex).toBe('0xe705');
   });
 
   describe('metadata', () => {
