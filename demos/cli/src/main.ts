f700f65d1d92cb441c6cea5ce2f7dff26ed2564e
VincentHoong <vyshoong9509@gmail.com>
2024-06-03 15:32:26 +0800

    revert: redundant commit

--- demos/cli/src/main.ts
+++ demos/cli/src/main.ts
@@ -15,43 +15,37 @@ async function main() {
     apiKey: env['MORALIS_API_KEY'],
   });
 
-  const data = await Moralis.EvmApi.nft.getNFTContractSalePrices({
-    address: '0xd2f668a8461d6761115daf8aeb3cdf5f40c532c6',
-    // tokenId: '572',
+  const block = await Moralis.EvmApi.block.getBlock({
+    blockNumberOrHash: '15305775',
+    chain: '0x1',
   });
-  console.log('nft sale prices', data);
+  console.log(block);
 
-  // const block = await Moralis.EvmApi.block.getBlock({
-  //   blockNumberOrHash: '15305775',
-  //   chain: '0x1',
-  // });
-  // console.log(block);
+  const blockStats = await Moralis.EvmApi.block.getBlockStats({
+    blockNumberOrHash: '18091265',
+  });
+  console.log('blockStats', blockStats.result.toJSON());
 
-  // const blockStats = await Moralis.EvmApi.block.getBlockStats({
-  //   blockNumberOrHash: '18091265',
-  // });
-  // console.log('blockStats', blockStats.result.toJSON());
+  const nftCollectionStats = await Moralis.EvmApi.nft.getNFTCollectionStats({
+    address: '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
+  });
+  console.log('nftCollectionStats', nftCollectionStats.result.toJSON());
 
-  // const nftCollectionStats = await Moralis.EvmApi.nft.getNFTCollectionStats({
-  //   address: '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
-  // });
-  // console.log('nftCollectionStats', nftCollectionStats.result.toJSON());
+  const nftTokenStats = await Moralis.EvmApi.nft.getNFTTokenStats({
+    address: '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
+    tokenId: '1',
+  });
+  console.log('nftTokenStats', nftTokenStats.result.toJSON());
 
-  // const nftTokenStats = await Moralis.EvmApi.nft.getNFTTokenStats({
-  //   address: '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
-  //   tokenId: '1',
-  // });
-  // console.log('nftTokenStats', nftTokenStats.result.toJSON());
+  const walletStats = await Moralis.EvmApi.wallets.getWalletStats({
+    address: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
+  });
+  console.log('walletStats', walletStats.result.toJSON());
 
-  // const walletStats = await Moralis.EvmApi.wallets.getWalletStats({
-  //   address: '0xd8da6bf26964af9d7eed9e03e53415d37aa96045',
-  // });
-  // console.log('walletStats', walletStats.result.toJSON());
-
-  // const tokenStats = await Moralis.EvmApi.token.getTokenStats({
-  //   address: '0x4a220e6096b25eadb88358cb44068a3248254675',
-  // });
-  // console.log('tokenStats', tokenStats.result.toJSON());
+  const tokenStats = await Moralis.EvmApi.token.getTokenStats({
+    address: '0x4a220e6096b25eadb88358cb44068a3248254675',
+  });
+  console.log('tokenStats', tokenStats.result.toJSON());
 }
 
 main();
