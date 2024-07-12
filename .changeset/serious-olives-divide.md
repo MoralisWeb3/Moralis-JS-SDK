d0dd0f6eece2e14da6945d8b9e02e9b24348b19a
gabkk <gab.kuma@gmail.com>
2024-07-12 16:57:32 +0800

    feat: Update getTopProfitableWalletPerToken return type

---
+++ .changeset/serious-olives-divide.md
@@ -0,0 +1,7 @@
+---
+'@moralisweb3/common-evm-utils': patch
+'@moralisweb3/evm-api': patch
+'moralis': patch
+---
+
+Update the return types of the `getTopProfitableWalletPerToken` method.
--- packages/common/evmUtils/src/generated/types/EvmNetWorthResult.ts
+++ packages/common/evmUtils/src/generated/types/EvmNetWorthResult.ts
@@ -1,4 +1,5 @@
 import { EvmChainNetWorth, EvmChainNetWorthInput, EvmChainNetWorthJSON } from '../types/EvmChainNetWorth';
+import { EvmUnavailableChainNetWorth, EvmUnavailableChainNetWorthInput, EvmUnavailableChainNetWorthJSON } from '../types/EvmUnavailableChainNetWorth';
 
 // $ref: #/components/schemas/netWorthResult
 // type: netWorthResult
@@ -6,17 +7,20 @@ import { EvmChainNetWorth, EvmChainNetWorthInput, EvmChainNetWorthJSON } from '.
 // - total_networth_usd ($ref: #/components/schemas/netWorthResult/properties/total_networth_usd)
 // - chains ($ref: #/components/schemas/chainNetWorth)
 // - unsupported_chain_ids ($ref: #/components/schemas/netWorthResult/properties/unsupported_chain_ids)
+// - unavailable_chains ($ref: #/components/schemas/unavailableChainNetWorth)
 
 export interface EvmNetWorthResultJSON {
   readonly total_networth_usd: string;
   readonly chains: EvmChainNetWorthJSON[];
   readonly unsupported_chain_ids?: string[];
+  readonly unavailable_chains?: EvmUnavailableChainNetWorthJSON[];
 }
 
 export interface EvmNetWorthResultInput {
   readonly totalNetworthUsd: string;
   readonly chains: EvmChainNetWorthInput[] | EvmChainNetWorth[];
   readonly unsupportedChainIds?: string[];
+  readonly unavailableChains?: EvmUnavailableChainNetWorthInput[] | EvmUnavailableChainNetWorth[];
 }
 
 export class EvmNetWorthResult {
@@ -32,6 +36,7 @@ export class EvmNetWorthResult {
       totalNetworthUsd: json.total_networth_usd,
       chains: json.chains.map((item) => EvmChainNetWorth.fromJSON(item)),
       unsupportedChainIds: json.unsupported_chain_ids,
+      unavailableChains: json.unavailable_chains ? json.unavailable_chains.map((item) => EvmUnavailableChainNetWorth.fromJSON(item)) : undefined,
     };
     return EvmNetWorthResult.create(input);
   }
@@ -45,11 +50,16 @@ export class EvmNetWorthResult {
    * @description The chain ids that are not supported
    */
   public readonly unsupportedChainIds?: string[];
+  /**
+   * @description The chains that are not available during the request
+   */
+  public readonly unavailableChains?: EvmUnavailableChainNetWorth[];
 
   private constructor(input: EvmNetWorthResultInput) {
     this.totalNetworthUsd = input.totalNetworthUsd;
     this.chains = input.chains.map((item) => EvmChainNetWorth.create(item));
     this.unsupportedChainIds = input.unsupportedChainIds;
+    this.unavailableChains = input.unavailableChains ? input.unavailableChains.map((item) => EvmUnavailableChainNetWorth.create(item)) : undefined;
   }
 
   public toJSON(): EvmNetWorthResultJSON {
@@ -57,6 +67,7 @@ export class EvmNetWorthResult {
       total_networth_usd: this.totalNetworthUsd,
       chains: this.chains.map((item) => item.toJSON()),
       unsupported_chain_ids: this.unsupportedChainIds,
+      unavailable_chains: this.unavailableChains ? this.unavailableChains.map((item) => item.toJSON()) : undefined,
     }
   }
 }
